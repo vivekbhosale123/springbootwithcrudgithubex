7b9d08dd889258e013024ecd392d1dcb2fd9f5f2
vivek Bhosale <vivekbhosale146@gmail.com>
2026-01-09 15:12:59 +0530

    initial commit

---
+++ springbootcrudwithgithubex/src/main/java/com/vdb/SpringbootcrudwithgithubexApplication.java
@@ -0,0 +1,13 @@
+package com.vdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudwithgithubexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudwithgithubexApplication.class, args);
+	}
+
+}
